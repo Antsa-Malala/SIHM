c549e26d31864587bfa81922cb728f6e8ced0454
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2023-07-08 12:32:21 -0700

    reset

--- application/views/Content.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <h2>Zany ndray ny content</h2>
-</div>
--- application/views/templates/template.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->load->view("templates/header");
-$this->load->view($contents,$client);
-$this->load->view("templates/footer");
-?>
