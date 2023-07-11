f1c389b5c4484479fab4524dca125ec38eb8ae4c
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2023-07-11 11:18:59 -0700

    Update Regimemodel.php

--- application/models/regime/Regimemodel.php
+++ application/models/regime/Regimemodel.php
@@ -3,21 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Regimemodel extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
     public function get_all_regime()
     {
 		$query = $this->db->query("select * from regime");
@@ -53,4 +38,55 @@ class Regimemodel extends CI_Controller {
 		}
 		return $result;
 	}
+	public function get_details_regime(){
+		$query = $this->db->query("SELECT DISTINCT v.id_regime , SUM(PRIX) as sum , poids , azo_perdu FROM 
+		(SELECT  rg.id_regime , rg.poids , rg.azo_perdu , pl.nom_plat , pl.disponibilite , pl.prix  , act.description_activite from regime_plat rpl
+			JOIN regime rg 
+			ON rpl.id_regime = rg.id_regime
+			JOIN plat pl
+			ON rpl.id_plat = pl.id_plat
+			JOIN activite act
+			ON rpl.id_activite = act.id_activite)
+		v GROUP BY v.id_regime
+		");
+		$results = array();
+		foreach( $query->result_array() as $row){
+			$results[] = $row; 
+		}
+		return $results;
+	}
+	public function get_regime_achetee_par_client($id_utilisateur)
+	{
+		$query = $this->db->query("select * from regime_achete where id_utilisateur=%s");
+		$query=sprintf($query,$this->db->escape($id_utilisateur));
+		$result = array();
+		foreach($query->result_array() as $row)
+		{
+			array_push($result,$row);
+		}
+		return $result;
+	}
+
+	public function get_one_regime($id){
+		$query = $this->db->get_where("regime" , array( "id_regime" => $id));
+		$result = null;
+		foreach( $query->result_array() as $row){
+			$result = $row;
+		}
+		return $result;
+	}
+
+	public function get_prix_total_one_regime($id_regime)
+	{
+		$query = $this->db->query("select SUM(prix) as prix_total from regime_plat join plat on regime_plat.id_plat=plat.id_plat where id_regime=%s");
+		$query=sprintf($query,$this->db->escape($id_regime));
+		$result = null;
+		foreach($query->result_array() as $row)
+		{
+			$result=$row;
+		}
+		return $result;	
+	}
+
+>>>>>>> Stashed changes
 }
