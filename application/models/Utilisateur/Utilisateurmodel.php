c7cdf6bb74bfc09f721cd5f2e75e1a0ae73bb7a4
DaisukeDitho <120396130+DaisukeDitho@users.noreply.github.com>
2023-07-11 06:59:26 +0200

    modification modifier_trait

--- application/models/Utilisateur/Utilisateurmodel.php
+++ application/models/Utilisateur/Utilisateurmodel.php
@@ -68,10 +68,27 @@
             return $result;
         }
 
-        public function modification_trait_utilisateur($nom,$prenom,$date_naissance,$genre,$mail,$mdp,$id)
+        public function modification_trait_utilisateur($nom,$prenom,$mail,$mdp,$taille,$poids,$id)
         {
-            $sql="update utilisateur set nom=%s,prenom=%s,datedenaissance=%s,genre=%s,mail=%s,mdp=%s where id_utilisateur=%s";
-            $sql=sprintf($sql,$this->db->escape($nom),$this->db->escape($prenom),$this->db->escape($date_naissance),$this->db->escape($genre),$this->db->escape($mail),$this->db->escape($mdp),$this->db->escape($id));
+            $sql="update utilisateur set nom=%s,prenom=%s,mail=%s,mdp=%s where id_utilisateur=%s";
+            $sql=sprintf($sql,$this->db->escape($nom),$this->db->escape($prenom),$this->db->escape($mail),$this->db->escape($mdp),$this->db->escape($id));
+            echo $sql;
+            $this->db->query($sql);
+
+            $sql="update taille set date_fin=now() where id_utilisateur=%s";
+            $sql=sprintf($sql,$this->db->escape($id));
+            $this->db->query($sql);
+
+            $sql="insert into taille values (%s,%s,now(),null)";
+            $sql=sprintf($sql,$this->db->escape($id),$this->db->escape($taille));
+            $this->db->query($sql);
+            
+            $sql="update poids set date_fin=now() where id_utilisateur=%s";
+            $sql=sprintf($sql,$this->db->escape($id));
+            $this->db->query($sql);
+
+            $sql="insert into poids values (%s,%s,now(),null)";
+            $sql=sprintf($sql,$this->db->escape($id),$this->db->escape($poids));
             $this->db->query($sql);
         }
 
@@ -88,9 +105,9 @@
         }
         public function get_objectif_now_utilisateur($id)
         {
-            $sql="select * from objectif where id_utilisateur=%s and date_fin=null";
+            $sql="select * from objectif where id_utilisateur=%s and date_fin is null";
             $sql=sprintf($sql,$this->db->escape($id));
-            // echo $sql;
+            //echo $sql;
             $query=$this->db->query($sql);
 
             $result=null;
