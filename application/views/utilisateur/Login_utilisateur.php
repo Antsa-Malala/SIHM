5b4d00a9f4da9e0dddd73a655fdef2ada2a40372
Antsa-Malala <malalanirina030304@gmail.com>
2023-07-11 14:41:12 +0300

    update

--- application/views/utilisateur/Login_utilisateur.php
+++ application/views/utilisateur/Login_utilisateur.php
@@ -50,13 +50,13 @@
                 <div class="card-body">
                     <div class="row">
                       <div class="mb-4">
-                          <div class="input-group input-group-outline my-3">
+                          <div class="input-group input-group-static my-3">
                               <label class="form-label">Email</label>
                               <input type="mail" class="form-control" name="mail" value="john.doe@example.com">
                           </div>
                       </div>
                       <div class="mb-4">
-                          <div class="input-group input-group-outline my-3">
+                          <div class="input-group input-group-static my-3">
                             <label class="form-label">Mot de passe</label>
                           <input type="password" class="form-control" name="mdp" value="password123">
                           </div>
